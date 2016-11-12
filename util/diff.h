07958d35ceddb4d5a0ab75e2d7790e30cbdb96aa
Jack Weinstein <jhsw93@gmail.com>
2016-11-12 14:41:00 -0500

    make memoization for differ a little bit more efficient by memoizing linked list nodes rather than entire lists of results

--- util/diff.h
+++ util/diff.h
@@ -1,9 +1,9 @@
-#include <deque>
 #include <tuple>
 #include <unordered_map>
 #include <vector>
 
 #include "util/one_of.h"
+#include "util/optional.h"
 
 namespace util {
 
@@ -31,23 +31,70 @@ class SequenceDiffer {
   };
 
   template <typename T>
-  std::deque<Modification> Diff(const T& lhs, const T& rhs) {
-    cached_seqs_.clear();
-    return Diff(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
-  }
+  std::vector<Modification> Diff(const T& lhs, const T& rhs);
 
  private:
-  std::deque<Modification> Diff(const It& lhs_begin, const It& lhs_end,
-                                const It& rhs_begin, const It& rhs_end);
-
-  std::deque<Modification> Recurse(const It& lhs_begin, const It& lhs_end,
-                                   const It& rhs_begin, const It& rhs_end);
-
   static std::tuple<std::size_t, std::size_t> ToCacheKey(const It& lhs_begin,
                                                          const It& lhs_end,
                                                          const It& rhs_begin,
                                                          const It& rhs_end) {
     return std::make_tuple(lhs_end - lhs_begin, rhs_end - rhs_begin);
+  }
+
+  struct ModSeqResult {
+    util::Optional<Modification> head;
+    const ModSeqResult* tail = nullptr;
+    std::size_t len = 0;
+  };
+
+  ModSeqResult Diff(const It& lhs_begin, const It& lhs_end, const It& rhs_begin,
+                    const It& rhs_end);
+
+  const ModSeqResult* Recurse(const It& lhs_begin, const It& lhs_end,
+                              const It& rhs_begin, const It& rhs_end) {
+    const auto& key = ToCacheKey(lhs_begin, lhs_end, rhs_begin, rhs_end);
+    auto it = cached_seqs_.find(key);
+    if (it == cached_seqs_.end()) {
+      it = cached_seqs_
+               .emplace(key, Diff(lhs_begin, lhs_end, rhs_begin, rhs_end))
+               .first;
+    }
+    return &(it->second);
+  }
+
+  ModSeqResult MakeModification(const It& lhs_begin, const It& lhs_end,
+                                const It& rhs_begin, const It& rhs_end,
+                                util::Optional<Modification> m) {
+    std::size_t len = 0;
+    const ModSeqResult* tail;
+    if (m.is_present()) {
+      tail = m.value().is_addition()
+                 ? Recurse(lhs_begin, lhs_end, rhs_begin + 1, rhs_end)
+                 : Recurse(lhs_begin + 1, lhs_end, rhs_begin, rhs_end);
+      len = 1 + tail->len;
+    } else {
+      tail = Recurse(lhs_begin + 1, lhs_end, rhs_begin + 1, rhs_end);
+      len = tail->len;
+    }
+    return ModSeqResult{std::move(m), tail, len};
+  }
+
+  ModSeqResult MakeAddition(const It& lhs_begin, const It& lhs_end,
+                            const It& rhs_begin, const It& rhs_end) {
+    return MakeModification(lhs_begin, lhs_end, rhs_begin, rhs_end,
+                            {Addition{lhs_begin, rhs_begin}});
+  }
+
+  ModSeqResult MakeDeletion(const It& lhs_begin, const It& lhs_end,
+                            const It& rhs_begin, const It& rhs_end) {
+    return MakeModification(lhs_begin, lhs_end, rhs_begin, rhs_end,
+                            {Deletion{lhs_begin}});
+  }
+
+  ModSeqResult MakeNoModification(const It& lhs_begin, const It& lhs_end,
+                                  const It& rhs_begin, const It& rhs_end) {
+    return MakeModification(lhs_begin, lhs_end, rhs_begin, rhs_end,
+                            util::EmptyOptional());
   }
 
   struct SeqCacheKeyHasher {
@@ -58,63 +105,59 @@ class SequenceDiffer {
     }
   };
 
-  std::unordered_map<
-      std::tuple<std::size_t, std::size_t>,
-      std::deque<Modification>,
-      SeqCacheKeyHasher>
+  std::unordered_map<std::tuple<std::size_t, std::size_t>, ModSeqResult,
+                     SeqCacheKeyHasher>
       cached_seqs_;
 };
 
 template <typename T>
-std::deque<typename SequenceDiffer<typename T::const_iterator>::Modification> Diff(
-    const T& lhs, const T& rhs) {
+std::vector<typename SequenceDiffer<typename T::const_iterator>::Modification>
+Diff(const T& lhs, const T& rhs) {
   return SequenceDiffer<typename T::const_iterator>().Diff(lhs, rhs);
 }
 
 template <typename It>
-std::deque<typename SequenceDiffer<It>::Modification> SequenceDiffer<It>::Recurse(
-    const It& lhs_begin, const It& lhs_end, const It& rhs_begin,
-    const It& rhs_end) {
-  const auto& key = ToCacheKey(lhs_begin, lhs_end, rhs_begin, rhs_end);
-  auto it = cached_seqs_.find(key);
-  if (it != cached_seqs_.end()) {
-    return it->second;
+template <typename T>
+std::vector<typename SequenceDiffer<It>::Modification> SequenceDiffer<It>::Diff(
+    const T& lhs, const T& rhs) {
+  cached_seqs_.clear();
+  const ModSeqResult* result_list =
+      Recurse(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+  std::vector<Modification> result;
+  while (result_list != nullptr) {
+    if (result_list->head.is_present()) {
+      result.push_back(result_list->head.value());
+    }
+    result_list = result_list->tail;
   }
-  return cached_seqs_[key] = Diff(lhs_begin, lhs_end, rhs_begin, rhs_end);
+  return result;
 }
 
 template <typename It>
-std::deque<typename SequenceDiffer<It>::Modification> SequenceDiffer<It>::Diff(
+typename SequenceDiffer<It>::ModSeqResult SequenceDiffer<It>::Diff(
     const It& lhs_begin, const It& lhs_end, const It& rhs_begin,
     const It& rhs_end) {
-  if (lhs_begin < lhs_end && rhs_begin < rhs_end) {
-    std::deque<Modification> remove_lhs =
-        Recurse(lhs_begin + 1, lhs_end, rhs_begin, rhs_end);
-    remove_lhs.push_front(Modification{Deletion{lhs_begin}});
-    std::deque<Modification> add_rhs =
-        Recurse(lhs_begin, lhs_end, rhs_begin + 1, rhs_end);
-    add_rhs.push_front(Modification{Addition{lhs_begin, rhs_begin}});
-    auto result = remove_lhs.size() < add_rhs.size() ? std::move(remove_lhs)
-                                                     : std::move(add_rhs);
-
-    if (*lhs_begin == *rhs_begin) {
-      std::deque<Modification> subseq =
-          Recurse(lhs_begin + 1, lhs_end, rhs_begin + 1, rhs_end);
-      if (subseq.size() < result.size()) {
-        result = std::move(subseq);
-      }
+  util::Optional<ModSeqResult> result;
+  if (lhs_begin < lhs_end) {
+    result = MakeDeletion(lhs_begin, lhs_end, rhs_begin, rhs_end);
+  }
+  if (rhs_begin < rhs_end) {
+    ModSeqResult add_rhs = MakeAddition(lhs_begin, lhs_end, rhs_begin, rhs_end);
+    if (!result.is_present() || add_rhs.len < result.value().len) {
+      result = std::move(add_rhs);
     }
-    return std::move(result);
   }
-
-  std::deque<Modification> result;
-  for (It i = rhs_begin; i != rhs_end; ++i) {
-    result.push_back(Modification{Addition{lhs_end, i}});
+  if (lhs_begin < lhs_end && rhs_begin < rhs_end && *lhs_begin == *rhs_begin) {
+    ModSeqResult no_mod =
+        MakeNoModification(lhs_begin, lhs_end, rhs_begin, rhs_end);
+    if (!result.is_present() || no_mod.len < result.value().len) {
+      result = std::move(no_mod);
+    }
   }
-  for (It i = lhs_begin; i != lhs_end; ++i) {
-    result.push_back(Modification{Deletion{i}});
+  if (result.is_present()) {
+    return std::move(result.mutable_value());
   }
-  return result;
+  return ModSeqResult{util::EmptyOptional(), nullptr, 0};
 }
 
 }  // namespace util
